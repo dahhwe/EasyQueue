e37ea5199eae8a2fefdf35cc906acd1e88205845
Dani <dahhwe@gmail.com>
2024-02-17 23:33:52 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const students = [
+    {id: 1, name: "Абишев Артем Владимирович"},
+    {id: 2, name: "Биликтуев Батор Эрдэмович"},
+    {id: 3, name: "Гарманов Константин Витальевич"},
+    {id: 4, name: "Демченко Артем Денисович"},
+    {id: 5, name: "Добросердова Анастасия Андреевна"},
+    {id: 6, name: "Исаев Руслан Саххатович"},
+    {id: 7, name: "Ковалев Игорь Евгеньевич"},
+    {id: 8, name: "Королевич Андрей Александрович"},
+    {id: 9, name: "Луговкин Егор Константинович"},
+    {id: 10, name: "Магеров Оскар Евгеньевич"},
+    {id: 11, name: "Мартышов Евгений Денисович"},
+    {id: 12, name: "Миллер Эрик Станиславович"},
+    {id: 13, name: "Михайлов Алексей Николаевич"},
+    {id: 14, name: "Надобных Дмитрий Николаевич"},
+    {id: 15, name: "Новиков Владимир Владимирович"},
+    {id: 16, name: "Периянина Ксения Олеговна"},
+    {id: 17, name: "Поярков Денис Дмитриевич"},
+    {id: 18, name: "Рус София Дмитриевна"},
+    {id: 19, name: "Смолянинов Ярослав Евгеньевич"},
+    {id: 20, name: "Терентьев Никита Александрович"},
+    {id: 21, name: "Удалов Егор Сергеевич"},
+    {id: 22, name: "Устинов Данил Егорович"},
+    {id: 23, name: "Фонград Анастасия Андреевна"},
+    {id: 24, name: "Шестера Дмитрий Данилович"},
+    {id: 25, name: "Шинкаренко Даниил Дмитриевич"},
+    {id: 26, name: "Якимович Владимир Максимович"}
+];
+
+function updateCurrentTime() {
+    const now = new Date();
+    const formattedDate = now.toLocaleString('ru-RU', { hour12: false });
+    document.getElementById('currentTime').textContent = formattedDate;
+}
+
+
+function loadStudents() {
+    fetch('https://raw.githubusercontent.com/dahhwe/EasyQueue/main/sequence.json')
+        .then(response => response.json())
+        .then(data => {
+            const sequence = data.result.random.data[0];
+            const queueElement = document.getElementById('studentsQueue');
+            sequence.forEach((number, index) => {
+                const student = students.find(s => s.id === number);
+                if (student) {
+                    const listItem = document.createElement('li');
+                    listItem.classList.add('list-group-item');
+                    listItem.textContent = `${index + 1}. ${student.name}`;
+                    queueElement.appendChild(listItem);
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Ошибка при загрузке последовательности: ', error);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    updateCurrentTime();
+    // Обновление времени каждую секунду
+    setInterval(updateCurrentTime, 1000);
+    loadStudents();
+});
